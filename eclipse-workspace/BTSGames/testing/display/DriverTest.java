3b5878602bac9e0d1cb674ec7de8352290c50958
jpdom <jdpalett@mtu.edu>
2018-10-25 16:26:06 -0400

    Added test classes

---
+++ eclipse-workspace/BTSGames/testing/display/DriverTest.java
@@ -0,0 +1,14 @@
+package display;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DriverTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ eclipse-workspace/BTSGames/testing/display/StatisticsMenuTest.java
@@ -0,0 +1,14 @@
+package display;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StatisticsMenuTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
