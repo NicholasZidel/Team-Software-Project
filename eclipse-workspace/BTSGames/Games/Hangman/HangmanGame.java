7517e191845a76e7d7f9dec5c9bd24f68e6af03b
mattk <mattk@DESKTOP-2LGV60P>
2018-11-26 23:17:50 -0500

    Hangman almost works

--- eclipse-workspace/BTSGames/Games/Hangman/HangmanGame.java
+++ eclipse-workspace/BTSGames/Games/Hangman/HangmanGame.java
@@ -28,26 +28,20 @@ import java.awt.event.ActionEvent;
 
 public class HangmanGame {
 
-	HangmanAI ai = new HangmanAI();  
+	static String[] words = {"understand","computer","java", "jake", "amount", "hangman",
+			"destroy","caterpillar","specific","why","goat","quantum","juno","sacrifice",
+			"lemonade","theatre","rocket","faucet","contemporary","loser","college","amphitheatre",
+			"sloth","magical","mistake","stratosphere"};
+	static int count = 0;
+	static int x = 0;
+	static int len = words.length;
+	static String  theWord;
+	
+	static String  newWord = "";
+	static String replaceWord = "-";
 	private JFrame frame;
 	private MyGraphics panel;
 
-	/**
-	 * Launch the application.
-	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					HangmanGame window = new HangmanGame();
-//					window.frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
-	
 	/**
 	 * Launch the application.
 	 */
@@ -71,7 +65,7 @@ public class HangmanGame {
 		//resetMan();
 		initialize();
 	}
-
+	
 	public void resetMan() {
 		head = false;
 		body = false;
@@ -79,48 +73,43 @@ public class HangmanGame {
 		Larm = false;
 		Rleg = false;
 		Lleg = false;
+		txtLabel.setText("");
+		newWord = "";
+		replaceWord = "-";
+		count = 0;
 	}
 	
-	//-----------------------------------------------------------------------------------------
-	//Graphics for hangman
+	public void wordPlacer() {
+		String w = new String("");
+		int num = (int) (Math.random()*len); //picks a random word
+		theWord = words[num];
+		for(int i = 0; i < theWord.length(); i++)
+		{
+			w = w + "-";
+			
+		}
+		wordLabel.setText(w);
+	}
 	
-	public boolean head, body, Rarm, Larm, Rleg, Lleg;		
-	MyGraphics hangmanGraphics = new MyGraphics();
+	private static String wordPlacer2() {
+		for(int i = 0; i < theWord.length(); i++)
+		{
+			newWord += replaceWord;
+		}
+		return newWord;
+	}
 	
-    public class MyGraphics extends JPanel{
-        private static final long serialVersionUID = 1L;
+	private static boolean checkLoss(int c) {
+		boolean loss = false;
+		if (c >= 6 )
+			loss = true;
+		return loss;
+	}
+	
 
-        MyGraphics() {
-            setPreferredSize(new Dimension(400, 400));
-        	//setOpaque(false);
-        }
-
-        @Override
-        public void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            Graphics2D g2d = (Graphics2D) g;
-    		g2d.setColor( Color.BLACK );
-    		g2d.fillRect(50, 150, 70, 10); 				//base horizontal
-    		g2d.fillRect(75, 50 , 10, 100);				//base vertical
-    		g2d.fillRect(75, 50, 75, 10);					//base top horizontal
-    		if (head)
-    			g2d.drawOval(130, 60, 20, 20); 			//head
-    		if (body)
-    			g2d.drawLine(140, 80, 140, 112);		//body
-    		if (Larm)
-    			g2d.drawLine(140, 85, 120, 108);		//left arm
-    		if (Rarm)
-    			g2d.drawLine(140, 85, 160, 108);		//right arm
-    		if (Rleg)
-    			g2d.drawLine(140, 112, 150, 135 );	//right leg
-    		if (Lleg)
-    			g2d.drawLine(140, 112, 130, 135 );	// left leg
-    		
-        }
-    }
-	//-----------------------------------------------------------------------------------------
-	static JLabel promptLabel, wordLabel, txtLabel;
-	
+	static JLabel promptLabel, wordLabel;
+	static JTextField txtLabel;
+	private static char letter;
 	private void initialize() {
 		frame = new JFrame();
 		frame.setTitle("Hangman");
@@ -149,11 +138,11 @@ public class HangmanGame {
 		lblHangman.setBounds(154, 0, 114, 38);
 		panel.add(lblHangman);
 		
-		promptLabel = new JLabel("promt");
+		promptLabel = new JLabel("prompt");
 		promptLabel.setBounds(302, 59, 118, 35);
 		panel.add(promptLabel);
 		
-		txtLabel = new JLabel("user text");
+		txtLabel = new JTextField("user text");
 		txtLabel.setBounds(302, 107, 75, 25);
 		panel.add(txtLabel);
 		
@@ -163,28 +152,144 @@ public class HangmanGame {
 		wordLabel.setBounds(25, 175, 352, 25);
 		panel.add(wordLabel);
 		
+		JButton submit = new JButton(">");
+		submit.setBackground(Color.BLACK);
+		submit.setForeground(Color.MAGENTA);
+		submit.setBounds(377, 107, 40, 25);
+		submit.setFont(new Font("Sitka Subheading", Font.PLAIN, 10));
+		submit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				letter = txtLabel.getText().charAt(0);
+				System.out.println("theWord: " + theWord + "  newWord: " + newWord);
+				promptLabel.setText("Enter a Letter");
+				x = 0;
+				for (int i = 0; i < theWord.length(); i++) {
+					if (letter == (theWord.charAt(i))) {
+						newWord = (newWord.substring(0,i) + letter + newWord.substring(i + 1, newWord.length()));
+						x = 1;
+					}
+				}
+				System.out.println("theWord: " + theWord + "   newWord: " + newWord);
+				wordLabel.setText(newWord);
+				if(x == 0) {
+					count++;
+					//System.out.print("strike " + count + ": ");
+					if (count == 1) {
+						setHead(true);
+						promptLabel.setText("strike " + count + ": ");
+					}
+					if (count == 2) {
+						setBody(true);
+						promptLabel.setText("strike " + count + ": ");
+					}
+					if (count == 3) {
+						setLarm(true);
+						promptLabel.setText("strike " + count + ": ");
+					}
+					if (count == 4) {
+						setRarm(true);
+						promptLabel.setText("strike " + count + ": ");
+					}
+					if (count == 5) {
+						setLleg(true);
+						promptLabel.setText("strike " + count + ": ");
+					}
+					if (count == 6) {
+						setRleg(true);
+						promptLabel.setText("strike " + count + ": ");
+					}
+					if (checkLoss(count)) {     								// if checkLoss is true, player has lost
+						 return;
+					}
+					//hangmanGraphics.repaint();
+					
+					//check win
+					if (theWord == wordLabel.getText()) {
+						promptLabel.setText("Nice Job!");
+						resetMan();
+					}
+				}
+			}
+		});
+		panel.add(submit);
+		
 		JButton replayButton = new JButton("Play");
 		replayButton.setBackground(Color.BLACK);
 		replayButton.setForeground(Color.MAGENTA);
 		replayButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				resetMan();
-				ai.wordPlacer();
-				ai.start();
+				promptLabel.setText("Enter a Letter");
+				wordPlacer();
+				newWord = wordPlacer2();
+				System.out.println("theWord: "  + theWord + "      newWord: " + newWord);
 			}
 		});
 		replayButton.setBounds(280, 213, 97, 25);
 		panel.add(replayButton);
 	}
 	
-	public static void setPromt(String prompt) {
-		promptLabel.setText(prompt);
+	//-----------------------------------------------------------------------------------------
+	//Graphics for hangman
+	
+	public static boolean head;
+	public static boolean body;
+	public static boolean Rarm;
+	public static boolean Larm;
+	public static boolean Rleg;
+	public static boolean Lleg;		
+	MyGraphics hangmanGraphics = new MyGraphics();
+	
+    public class MyGraphics extends JPanel{
+        private static final long serialVersionUID = 1L;
+
+        MyGraphics() {
+            setPreferredSize(new Dimension(400, 400));
+        	//setOpaque(false);
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g;
+    		g2d.setColor( Color.BLACK );
+    		g2d.fillRect(50, 150, 70, 10); 				//base horizontal
+    		g2d.fillRect(75, 50 , 10, 100);				//base vertical
+    		g2d.fillRect(75, 50, 75, 10);					//base top horizontal
+    		if (head)
+    			g2d.drawOval(130, 60, 20, 20); 			//head
+    		if (body)
+    			g2d.drawLine(140, 80, 140, 112);		//body
+    		if (Larm)
+    			g2d.drawLine(140, 85, 120, 108);		//left arm
+    		if (Rarm)
+    			g2d.drawLine(140, 85, 160, 108);		//right arm
+    		if (Rleg)
+    			g2d.drawLine(140, 112, 150, 135 );		//right leg
+    		if (Lleg)
+    			g2d.drawLine(140, 112, 130, 135 );		// left leg
+        }
+    }
+    
+	//-----------------------------------------------------------------------------------------
+	public static void setHead(boolean b) { //head
+		head = b;
 	}
-	public static void setTxt(String usertxt) {
-		txtLabel.setText(usertxt);
+	public static void setBody(boolean b) { //body
+		body = b;
 	}
-	public static void setWord(String word) {
-		wordLabel.setText(word);
+	public static void setLarm(boolean b) { //left arm
+		Larm = b;
+	}
+	public static void setRarm(boolean b) {//right arm
+		Rarm = b;
+	}
+	public static void setLleg(boolean b) { //left leg
+		Lleg = b;
+	}
+	public static void setRleg(boolean b) { //right leg
+		Rleg = b;
 	}
 	
 }
